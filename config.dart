4f0a56270628deb247a20b935b492097a70f9712
elienzzz <zfany.04@gmail.com>
2024-12-31 18:59:43 +0700

    Delete config.dart

--- config.dart
+++
@@ -1,4 +0,0 @@
-String token = '6773b664f853312de5509c6d';
-String project = 'cafeite';
-String appid = '6773b87bf853312de5509c73';
-String fileUri = 'https://file.etter.cloud/dd226fd9f5fcf8bc3cbdff22e2bd79efe';
