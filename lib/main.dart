7e69fae10e8f2240e0adbdc6f504761f8989bbde
elienzzz <zfany.04@gmail.com>
2024-12-31 19:30:02 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+/*
+  NRP: 992024007
+  Nama: Zilfany
+  Deskripsi Kode: 
+  Tanggal kode dibuat:
+*/
+
+import 'package:flutter/material.dart';
+
+import 'package:cafeite/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'cafeITe',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MakananList(),
+      routes: {
+        'list': (context) => const MakananList(),
+        // 'form_add' : (context) => const ReminderFormAdd()
+      },
+    );
+  }
+}
